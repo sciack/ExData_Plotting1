adf2e25ac1f70c9cc4d45bf7ce6ef83c4e021582
Mirko Sciachero <m.sciachero@replyltd.co.uk>
2015-08-07 17:16:17 +0100

    Plot1.R exploratory graph, should be refined

---
+++ plot1.R
@@ -0,0 +1,10 @@
+plot1 <- function() {
+  household_data <- read.csv("household_power_consumption.txt",sep=";",stringsAsFactors = FALSE)
+  household_data$Time <- strptime(paste(household_data$Date," ", household_data$Time),format="%d/%m/%Y %H:%M:%S")
+  household_data$Date <- as.Date(household_data$Date,format="%d/%m/%Y")
+  myData <- subset(subset(household_data, Date >= as.Date("01-02-2007", format="%d-%m-%Y")), Date <= as.Date("02-02-2007", format="%d-%m-%Y"))
+  myData$Global_active_power <- as.numeric(myData$Global_active_power)
+  png("plot1.png",width = 480, height = 480)
+  hist(myData$Global_active_power, main = "Global active power", col = "red",xlab = "Global Active Power (kilowatts)")
+  dev.off()
+}
