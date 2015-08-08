c99c7613b95d161c5e3544e22c26048ea7148564
Mirko Sciachero <m.sciachero@replyltd.co.uk>
2015-08-08 11:13:19 +0100

    Plot2 - 3 and 4
    Implementation of Plot 2 & 3 Plot 4 is only a template.

---
+++ plot3.R
@@ -0,0 +1,17 @@
+plot3 <- function() {
+  household_data <- read.csv("household_power_consumption.txt",sep=";",stringsAsFactors = FALSE)
+  household_data$Time <- strptime(paste(household_data$Date," ", household_data$Time),format="%d/%m/%Y %H:%M:%S")
+  household_data$Date <- as.Date(household_data$Date,format="%d/%m/%Y")
+  myData <- subset(subset(household_data, Date >= as.Date("01-02-2007", format="%d-%m-%Y")), Date <= as.Date("02-02-2007", format="%d-%m-%Y"))
+  myData$Sub_metering_1 <- as.numeric(myData$Sub_metering_1)
+  myData$Sub_metering_2 <- as.numeric(myData$Sub_metering_2)
+  myData$Sub_metering_3 <- as.numeric(myData$Sub_metering_3)
+  png("plot3.png",width = 480, height = 480)
+  
+  with(myData,plot(Time,Sub_metering_1,type="n", xlab ="",ylab = "Energy sub metering"))
+  with(myData,lines(Time,Sub_metering_1, col ="black"))
+  with(myData,lines(Time,Sub_metering_2, col ="red"))
+  with(myData,lines(Time,Sub_metering_3, col ="blue"))
+  legend("topright",lty = 1,col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  dev.off()
+}
